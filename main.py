0bc3269f5cdd58c4a829dba9d18ed8f9ded43350
Kevin Campos <kvnelpdfw@hotmail.com>
2022-06-21 23:08:38 -0500

    Project v1

--- main.py
+++ main.py
@@ -1,31 +1,30 @@
-MENU = {
-    "espresso": {
-        "ingredients": {
-            "water": 50,
-            "coffee": 18,
-        },
-        "cost": 1.5,
-    },
-    "latte": {
-        "ingredients": {
-            "water": 200,
-            "milk": 150,
-            "coffee": 24,
-        },
-        "cost": 2.5,
-    },
-    "cappuccino": {
-        "ingredients": {
-            "water": 250,
-            "milk": 100,
-            "coffee": 24,
-        },
-        "cost": 3.0,
-    }
-}
+from menu import MENU
 
-resources = {
-    "water": 300,
-    "milk": 200,
-    "coffee": 100,
-}
+print(MENU)
+
+
+
+# Coin Operated Machine
+# Penny = 1 cent - 0.01
+# Nickel = 5 cents - 0.05
+# Dime = 10 cents - 0.10
+# Quarter = 25 cents - 0.25
+
+# 1.
+# Print Report using 'report' to show what resources it has left.
+
+# 2.
+# Check resources are sufficient when order is provided.
+# It will ask how many of each coin to add, then provide a change amount
+# If no resources to make drink, it should state that.
+
+# 3.
+# It will ask how many of each coin to add, then provide a change amount
+# If not enough money is provided, all money is refunded and no drink given.
+
+# 4.
+# check that transaction is successful.
+
+# 5.
+# if good. make coffee, and reduce resources available.
+
