d7d3936f2947d81d9a598146e68b8e9eb347021c
Kevin Campos <kvnelpdfw@hotmail.com>
2022-06-22 21:26:03 -0500

    Project Complete.

--- main.py
+++ main.py
@@ -45,9 +45,21 @@ def shut_off():
     exit()
 
 
+def fetching_drink():
+    print("Fetching drink ingredients")
+    animation = ["-", " -", "  -"]
+
+    for i in range(100):
+        sleep(0.1)
+        sys.stdout.write("\r" + animation[i % len(animation)])
+        sys.stdout.flush()
+    #
+
+
+
 def left_stock():
     if resources == 0:
-        print("No more stock left for this action.")
+        print("Alert: Machine has no more ingredients left for this drink order\n")
         refill = input("Do you want to get refill info? ").lower().strip()
         if refill == "yes":
             print("\nIngredients remaining are:\n")
@@ -56,10 +68,18 @@ def left_stock():
                     print("{}: {}g".format(key, value).capitalize())
                     continue
                 print("{}: {}ml".format(key, value).capitalize())
+            turn_off_coffee: input("Do you want to power off machine?").lower()
+            if turn_off_coffee == "yes":
+                print("Coffee Machine will now power-off...")
+                shut_off()
         else:
-            exit()
-        print("Coffee Machine will now power-off...")
-        shut_off()
+            turn_off_coffee: input("Do you want to power off machine?").lower()
+            if turn_off_coffee == "yes":
+                print("Coffee Machine will now power-off...")
+                shut_off()
+
+    else:
+        print(f"Your {user_choice} is Ready. Enjoy!!!")
 
 
 def report():
@@ -75,6 +95,9 @@ def report():
 def item_price(drink, quarter, dime, nickel, pennie):
     """Provides Cost of Drink, Difference in price"""
     global money_in_machine
+    if money_in_machine < 0:
+        print("Not enough money in machine.")
+        exit()
     # Gives cost of Drink and subtracts money given
     item_cost = MENU[drink]["cost"]
     # print(f"\n{drink} costs: ${item_cost}")
@@ -92,22 +115,38 @@ def item_price(drink, quarter, dime, nickel, pennie):
 
     if money_inserted > item_cost:
         difference = money_inserted - item_cost
-        print(f"Here is your refund of ${money_inserted - item_cost}")
-        money_in_machine += difference
+        print(f"Here is your refund of ${format(difference, '.2f')}")
+        money_in_machine += item_cost
+        fetching_drink()
+        return "success"
+
+    elif money_inserted == item_cost:
+        print(money_in_machine)
+        money_in_machine += item_cost
+        print(money_in_machine)
+        fetching_drink()
+        return "success"
+
     else:
-        myFloat = item_cost - money_inserted
+        myfloat = item_cost - money_inserted
         print("That's not enough money.\n")
-        print(f"You inserted ${money_inserted} and are missing ${format(myFloat, '.2f')}\nYour money wil be refunded.")
+        print(f"You inserted ${money_inserted} and are missing ${format(myfloat, '.2f')}\nYour money will be refunded.")
         print("\nTry again, please.")
-        return 0
+        return "fail"
 
 
 machine_on = True
 
 while machine_on:
-    user_choice = input("What would you like? (espresso/latte/cappuccino) ").lower()
-    while user_choice not in ["report", "latte", "espresso", "cappuccino"]:
-        break
+    flag = True
+    while flag:
+        user_choice = input("What would you like? (espresso/latte/cappuccino) ").lower()
+        if user_choice not in ["report", "latte", "espresso", "cappuccino"]:
+            print("That's not valid entry, try again.")
+            continue
+        else:
+            flag = False
+
     if user_choice == "report":
         report()
         # Add a clear function/module so this report disappears after a delay
@@ -124,9 +163,16 @@ while machine_on:
 
     if user_choice == "espresso":
         # print("espresso")
+        item_price(user_choice, quarters, nickels, dimes, pennies)
         check_resources()
         resources, test = check_resources()
+        if test == 0:
+            print("Purchase Successful...Testing")
         left_stock()
+        # if item_price(user_choice, quarters, nickels, dimes, pennies) == "success".lower():
+        #     print(f"Here is your {user_choice}. Enjoy!")
+        # might delete bottom and move to report only...
+        print("Current Money in Machine: $", money_in_machine)
 
     elif user_choice == "latte":
         # print("latte")
@@ -140,9 +186,13 @@ while machine_on:
 
     elif user_choice == "cappuccino":
         # print("cappuccino")
+        item_price(user_choice, quarters, nickels, dimes, pennies)
         check_resources()
         resources, test = check_resources()
         left_stock()
+
+        # might delete bottom and move to report only...
+        print("Current Money in Machine: $", money_in_machine)
 
     elif user_choice == "off":
         choice = input("Are you sure you want to power-off? ").lower()
@@ -153,11 +203,6 @@ while machine_on:
             continue
     else:
         print("That's not a valid selection, try again.")
-
-    # resources = check_resources()
-    # if check_resources() == "No more Stock":
-    #     print("Testing for empty stock")
-    #     exit()
 
 # 1.
 # Print Report using 'report' to show what resources it has left.
